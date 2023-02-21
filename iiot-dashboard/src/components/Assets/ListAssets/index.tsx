2c5bd4215ddf067c671129830df081779ef97522
marcmec <viniciuslag@gmail.com>
2023-02-21 01:51:30 -0300

    style: create allassets to show assets

---
+++ iiot-dashboard/src/components/Assets/ListAssets/index.tsx
@@ -0,0 +1,54 @@
+import { Card, List } from "antd";
+import { useContext, useEffect, useState } from "react";
+import { API } from "../../../api/axios";
+import CompanyContext from "../../../contexts/Company";
+import { IAssets } from "../../../interfaces/Assets";
+import { InputSearch } from "../../InputSearch";
+import { CardAssetEspecification } from "../Card/CardAssetEspecifications";
+
+export const AllAssets = () => {
+    const { companyInfo } = useContext(CompanyContext);
+    const getCompanyInfoLocalStorage = localStorage.getItem("companyInfo");
+    const [allAssets, setAllAssets] = useState<IAssets[]>([]);
+
+    const GetAllAssets = async () => {
+        const { data } = await API.get("/assets");
+        const assetsCompanyFilter = data.filter(
+            (item: IAssets) =>
+                item.companyId === companyInfo?.id ||
+                item.companyId === JSON.parse(getCompanyInfoLocalStorage!).id
+        );
+
+        setAllAssets(assetsCompanyFilter);
+    };
+
+    useEffect(() => {
+        GetAllAssets();
+    }, []);
+    return (
+        <>
+            <Card title={"All Assets"}>
+                <InputSearch />
+
+                <List
+                    grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 2,
+                        md: 2,
+                        lg: 2,
+                        xl: 2,
+                        xxl: 2,
+                    }}
+                    dataSource={allAssets}
+                    pagination={{ pageSize: 2 }}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <CardAssetEspecification asset={item} />
+                        </List.Item>
+                    )}
+                />
+            </Card>
+        </>
+    );
+};
